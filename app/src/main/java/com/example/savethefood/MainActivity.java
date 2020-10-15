345e78d5473b1372e3cd84e85a0643fa77131ed2
Robbe Elsermans <robels@outlook.com>
2020-10-15 09:55:57 +0200

    initial dependensies

--- app/src/main/java/com/example/savethefood/MainActivity.java
+++ app/src/main/java/com/example/savethefood/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.savethefood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mresultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mresultText = findViewById(R.id.text_result);
+    }
+
+    public void Scan_btn(View view)
+    {
+
     }
 }
