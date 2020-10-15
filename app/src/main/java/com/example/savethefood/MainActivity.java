676a5bb465f93dd495befb33151441d81af250ad
Robbe Elsermans <robels@outlook.com>
2020-10-15 10:36:41 +0200

    werkt niet

--- app/src/main/java/com/example/savethefood/MainActivity.java
+++ app/src/main/java/com/example/savethefood/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.savethefood;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import org.w3c.dom.Text;
 
@@ -18,10 +27,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mresultText = findViewById(R.id.text_result);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED); //permissie aanvragen
+    }
+    public void Scan_btn(View view){
+        new IntentIntegrator(this).initiateScan(); // `this` is the current Activity
     }
 
-    public void Scan_btn(View view)
-    {
-
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
